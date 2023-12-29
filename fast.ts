628d76203c9e56393c351d9d734f2ea7e16fc373
roistaff <roistaff1983@gmail.com>
2023-12-29 12:33:18 +0900

    update

--- fast.ts
+++ fast.ts
@@ -1,6 +1,6 @@
 import * as mod from "https://deno.land/std@0.210.0/fmt/colors.ts";
 let start = Date.now()
-const response = await fetch("https://github.com/roistaff/fast/raw/master/test50M")
+const response = await fetch("https://github.com/roistaff/fast/raw/master/test40M")
 let end = Date.now()
 const time = (end - start)/1000
 const size = response.headers.get('Content-Length') / (1024 * 1024 );
